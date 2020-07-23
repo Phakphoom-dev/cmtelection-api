5f7b8563fcb148d31395ed3c6c9068fe65d3b8f4
Phakphoom Khumpuan <phakphoomit411@gmail.com>
2020-07-23 18:45:28 +0700

    Fix api

--- stdrouter.js
+++ stdrouter.js
@@ -17,12 +17,13 @@ router.get("/:id/:stdType", (req, res) => {
   if (stdType === "M") {
     Metro.find({ id: req.params.id }).exec((err, data) => {
       if (err) return res.status(400).send(err);
+      console.log(data);
       res.status(200).send(data);
     });
   } else {
     CCC.find({ id: req.params.id }).exec((err, data) => {
       if (err) return res.status(400).send(err);
-      res.status(200).send(data);
+      res.json({ response: data[0] });
     });
   }
 });
